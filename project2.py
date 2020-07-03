d3e95a4901f463437ec3194bbc36c6872bbd9f11
VarunBenjamin <66789571+VarunBenjamin@users.noreply.github.com>
2020-07-03 17:44:22 +0530

    Add files via upload

---
+++ project2.py
@@ -0,0 +1,16 @@
+#Assigning elements to different lists
+listt=['Alice','Bob','Charlie']
+print('My list is: ',listt)
+
+
+#Accessing elements from a tuple
+tup=('Alice','Bob','Charlie')
+print('My tuple is: ',tup)
+print('Tuple at index 1 is: ',tup[1])
+
+
+#Deleting different dictionary elements
+dic={'Alice':21,'Bob':22,'Charlie':21}
+print('My dictionary is:',dic)
+del dic['Alice']
+print('The dictionary after deleting a name is: ',dic)
